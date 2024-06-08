0344e76fd500f6352ac0cb83f838e1c61a8f332f
aljary12 <ryadisp@gmail.com>
2024-06-08 16:48:21 +0800

    feat: add async-storage for persist state

--- src/stores/history-store.ts
+++ src/stores/history-store.ts
@@ -1,20 +1,30 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware';
 
 interface HistoryState {
   cities: string[];
   addCity: (city: string) => void;
 }
 
-export const useHistoryState = create<HistoryState>()(set => ({
-  cities: [],
-  addCity: city =>
-    set(state => {
-      let cities = state.cities.concat(city);
+export const useHistoryState = create<HistoryState>()(
+  persist(
+    set => ({
+      cities: [],
+      addCity: city =>
+        set(state => {
+          let cities = state.cities.concat(city);
 
-      if (cities.length > 5) {
-        cities = cities.slice(1);
-      }
+          if (cities.length > 5) {
+            cities = cities.slice(1);
+          }
 
-      return {cities: cities};
+          return {cities: cities};
+        }),
     }),
-}));
+    {
+      name: 'history-store', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
